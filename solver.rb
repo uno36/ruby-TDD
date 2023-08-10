146d94cf96b30130b1a7c1369e3fe355a5498972
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-08-10 11:47:46 +0100

    FIX linter errors

--- solver.rb
+++ solver.rb
@@ -13,13 +13,13 @@ class Solver
   end
 
   def fizzbuzz(num)
-    if num % 3 == 0 && num % 5 == 0
+    if (num % 3).zero? && (num % 5).zero?
       'fizzbuzz'
-    elsif num % 3 == 0
+    elsif (num % 3).zero?
       'fizz'
-    elsif num % 5 == 0
+    elsif (num % 5).zero?
       'buzz'
-    else 
+    else
       num.to_s
     end
   end
