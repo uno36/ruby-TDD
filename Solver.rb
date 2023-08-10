a389ae7c6f9e8fdb23589e987dc2865f4866b21f
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-08-10 11:27:26 +0200

    Make the factorial method

--- Solver.rb
+++ Solver.rb
@@ -1,2 +1,11 @@
-class solver
+class Solver
+  def factorial(n)
+    raise ArgumentError, "Factorial argument must be a non-negative integer" if n < 0
+
+    return 1 if n == 0
+
+    result = 1
+    (1..n).each { |i| result *= i }
+    result
+  end
 end
--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -1,20 +1,20 @@
-require_relative '../solver'
-
-Rspecc.describe solver do 
+require_relative '../Solver'
+RSpec.describe Solver do
   describe '#factorial' do
-    it 'reurns 1 when given 0' do
-      solver = solver.new
+    it 'returns 1 when given 0' do
+      solver = Solver.new
       expect(solver.factorial(0)).to eq(1)
     end
 
-    it 'returns the correct factorialfor positive integer' do
-      solver = solver.new
-      epect(solver.factorial(5)).to eq(120)
+    it 'returns the correct factorial for positive integers' do
+      solver = Solver.new
+      expect(solver.factorial(5)).to eq(120)
       expect(solver.factorial(10)).to eq(3628800)
     end
 
     it 'raises an ArgumentError for negative integers' do
-      solver = solver.new
-      expect(solver.factorial(-5)).to raise_error(ArgumentError, 'Factorial Argument should be a non-negative integer')
-    end  
+      solver = Solver.new
+      expect { solver.factorial(-5) }.to raise_error(ArgumentError, 'Factorial argument must be a non-negative integer')  # Corrected error message
+    end
+  end
 end
